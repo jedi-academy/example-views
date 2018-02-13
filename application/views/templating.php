b599cc0f4e16d9696d7be3b48d166ed1cf145d50
Jim Parry <jim_parry@bcit.ca>
2018-02-13 13:41:04 -0800

    Update dwoo.org link

--- application/views/templating.php
+++ application/views/templating.php
@@ -5,7 +5,7 @@
 		<ul>
 			<li><a href="http://www.codeigniter.com/user_guide/libraries/parser.html" target="vdemo">CI Template Parser</a></li>
 			<li><a href="https://laravel.com/docs/5.3/blade" target="vdemo">Blade</a></li>
-			<li><a href="http://dwoo.org/documentation/1.2.x/" target="vdemo">Dwoo</a></li>
+			<li><a href="http://dwoo.org/documentation/v1.3/" target="vdemo">Dwoo</a></li>
 			<li><a href="http://www.smarty.net/docs/en/" target="vdemo">Smarty</a></li>
 			<li><a href="http://twig.sensiolabs.org/documentation" target="vdemo">Twig</a></li>
 		</ul>
